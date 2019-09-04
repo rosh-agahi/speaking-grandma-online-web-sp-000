6f640eb38a49314f03bd3177995608f0f5de1406
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 20:14:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(statement)
+  if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif
-    phrase == phrase.upcase 
+    statement == statement.upcase 
     return "NO, NOT SINCE 1938!" 
   else 
     return "HUH?! SPEAK UP, SONNY!"
