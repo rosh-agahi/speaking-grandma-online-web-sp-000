8becc111fe78fdb4bbdcb430573595702c2e04de
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 20:06:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif
     statement == statement.upcase 
     puts "NO, NOT SINCE 1938!" 
