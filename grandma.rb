cc18b87f059663563f7d29eef86ce815ea26bacb
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 19:55:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 statement = "Hi, Grandma!"
 
 def speak_to_grandma(statement)
-  if statement.match(/[A-Z]+\W$/)
+  if statement.match(/[A-Z]+\W$/) == TRUE
     puts "NO, NOT SINCE 1938!"
   elsif statement = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU YOO PUMPKIN"
