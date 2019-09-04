c335b4e76cc990b16372bf65c27c3bcb2fda06df
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-04 19:59:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,12 +3,11 @@
 statement = "Hi, Grandma!"
 
 def speak_to_grandma(statement)
-  if statement.match(/[A-Z]+\W$/) == TRUE
-    puts "NO, NOT SINCE 1938!"
-  elsif statement = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU YOO PUMPKIN"
-  else 
-    puts "HUH?! SPEAK UP, SONNY!"
+  if statement = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN"
+  else
+    statement.upcase ? "NO, NOT SINCE 1938!" : "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
